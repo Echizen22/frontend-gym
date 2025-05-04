1012f1e4a38ac5b43aca046bd3fce0919fcbc80b
Echizen22 <erixon.arce.justiniano@gmail.com>
2025-05-04 22:30:12 +0200

    Limipieza de layout que no uso.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 
 import { PUBLIC_ROUTES } from './public/public-routes';
 import { ADMIN_ROUTES } from './admin/admin-routes';
-import { PrivateLayoutComponent } from './layouts/private-layout/private-layout.component';
 import { USER_ROUTES } from './user/user-routes';
 import { authAdminGuard, authGuard } from './guards/auth.guard';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
